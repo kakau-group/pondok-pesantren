ed37bd10c16737e7854730789c4af4500eb6a570
fadjar setiawan <fadjarsetiawans@gmail.com>
2021-03-18 11:13:53 +0700

    commit awal

---
+++ formulir-pendaftaran.php
@@ -0,0 +1,16 @@
+<?php
+include("atas.php");
+?>
+
+
+
+
+
+
+<?php
+include("contact.php");
+?>
+
+<?php
+include("bawah.php");
+?>
---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+include("atas.php");
+?>
+
+<?php
+include("jumbotron.php");
+?>
+
+    <!-- ======= Pembuka Section ======= -->
+        <section id="contact" class="contact text-center">
+            <div class="container">
+                <div class="fdrs">
+                    <h2>السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ</h2>
+                    <p>Puji syukur kami panjatkan ke hadirat Allah SWT yang atas berkat rahmat dan hidayah-Nya kami bisa meluncurkan situs web Pondok Pesantren Al Hamid ini di Internet. Situs web ini bertujuan untuk memperkenalkan Al Hamid sebagai lembaga pendidikan dengan memanfaatkan media teknologi internet.</p>
+                    </div>
+                </div>
+            </section><!-- End Pembuka Section -->
+
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team section-bg text-center">
+        <div class="container">
+
+            <div class="fdrs">
+            <h2>Jenjang Pendidikan</h2>
+            </div>
+
+            <div class="row">
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                <div class="member">
+                <img src="assets/img/team/team-1.jpg" >
+                <h4 href="#">TK Islam</h4>
+                <p>
+                Taman bermain dan TK Islam Al Hamid adalah lembaga pendidikan pra-sekolah yang mengutamakan nilai-nilai islami...
+                </p>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                <div class="member">
+                <img src="assets/img/team/team-2.jpg" alt="">
+                <h4>Madrasah Ibtidaiyah</h4>
+                <p>
+                Lembaga pendidikan yang telah mempunyai sejarah panjang mengawali reputasinya sendiri dengan lengkap menciptakan tradisi. Tradisi...
+                </p>
+                </div>
+            </div>
+
+                        <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                <div class="member">
+                <img src="assets/img/team/team-2.jpg" alt="">
+                <h4>Madrasah Tsanawiyah</h4>
+                <p>
+                Perkembangan era modernisasi pada saat ini adakalanya mengesampingkan nilai keagamaan yang dirasa cukup meresahkan. Untuk...
+                </p>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                <div class="member">
+                <img src="assets/img/team/team-3.jpg" alt="">
+                <h4>Madrasah Aliyah</h4>
+                <p>
+                Membina insan yang beriman, bertaqwa kepada Allah SWT, kreatif, mandiri dan berorientasi masa depan dengan...
+                </p>
+                </div>
+            </div>
+
+            </div>
+
+        </div>
+        </section><!-- End Team Section -->
+
+
+
+<?php
+include("contact.php");
+?>
+
+<?php
+include("bawah.php");
+?>

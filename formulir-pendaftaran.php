01c298e20a2fca1bd1d89d6f479caccfb03f49db
fadjar setiawan <fadjarsetiawans@gmail.com>
2021-03-20 12:03:47 +0700

    tambah formulir

--- formulir-pendaftaran.php
+++ formulir-pendaftaran.php
@@ -2,15 +2,34 @@
 include("atas.php");
 ?>
 
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Kontak</h2>
+          <p>Silahkan hubungi kami jika ada yang kurang jelas</p>
+        </div>
+
+        <div class="row">
+
+          <div  class="col-lg-100% d-flex align-items-stretch">
+            <div class="info">
 
 
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeRzbXUgpwMUK3t0ipo3azjlncdM8dPQe-e26IU923GS0qdDA/viewform?embedded=true" width="100%" height="1795" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </div>
 
+          </div>
 
+        </div>
 
-<?php
-include("contact.php");
-?>
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
 
 <?php
 include("bawah.php");
 ?>
+
